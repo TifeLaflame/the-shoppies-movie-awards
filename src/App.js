f199739e5b859e0d6ffdf3b22fffda45afdec886
Tife <obideyib@gmail.com>
2021-01-08 01:49:07 -0800

    test yarn deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React today, be your best self
         </a>
       </header>
     </div>
